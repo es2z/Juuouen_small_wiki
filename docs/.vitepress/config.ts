9fce2997bb09ab8402471e0b802ea90e8a8bd965
es2z <es2@live.jp>
2024-09-08 16:21:54 +0900

    add dist folder

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,4 +1,24 @@
-import { defineConfig } from 'vitepress'
+import { defineConfig } from 'vitepress';
 
-// https://vitepress.vuejs.org/config/app-configs
-export default defineConfig({})
+export default defineConfig({
+    // サイト全体の基本情報
+    title: '獣くんきれはし', // サイトのタイトル
+    description: 'A small wiki for Juuouen', // サイトの説明
+
+    // ベースURLの設定 (GitHub Pagesの場合、リポジトリ名を指定)
+    base: '/Juuouen_small_wiki/',
+
+    // ヘッダーの設定
+    themeConfig: {
+        // サイドバーの設定
+        sidebar: [
+            {
+                text: 'Guide',
+                items: [
+                    { text: 'Introduction', link: '/overview/introduction' }, // ガイドの「導入」ページ
+
+                ]
+            }
+        ]
+    }
+});
